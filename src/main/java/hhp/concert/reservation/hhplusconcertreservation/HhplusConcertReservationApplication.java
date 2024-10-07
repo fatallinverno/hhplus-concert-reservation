c57c9d3f149ae9ced54a668620aea8bcd865f247
Linverno <fatal_linverno@naver.com>
2024-10-07 17:17:45 +0900

    initial setting

---
+++ src/main/java/hhp/concert/reservation/hhplusconcertreservation/HhplusConcertReservationApplication.java
@@ -0,0 +1,13 @@
+package hhp.concert.reservation.hhplusconcertreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusConcertReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusConcertReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hhp/concert/reservation/hhplusconcertreservation/HhplusConcertReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package hhp.concert.reservation.hhplusconcertreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusConcertReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
