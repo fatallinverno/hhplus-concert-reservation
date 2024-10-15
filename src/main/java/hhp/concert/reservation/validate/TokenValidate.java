34f22652ffd62f19f852e94606695f2ee0944715
Linverno <fatal_linverno@naver.com>
2024-10-16 00:46:09 +0900

    TokenValidate 수정

--- src/main/java/hhp/concert/reservation/validate/TokenValidate.java
+++ src/main/java/hhp/concert/reservation/validate/TokenValidate.java
@@ -1,19 +1,32 @@
 package hhp.concert.reservation.validate;
 
 import hhp.concert.reservation.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+@Component
 public class TokenValidate {
 
     private JwtUtil jwtUtil;
 
     public boolean validateToken(String token) {
-
         if(token != null) {
-            return jwtUtil.isTokenValid(token);
+            return isTokenValid(token);
         } else {
             throw new IllegalArgumentException("token값이 없습니다.");
         }
+    }
 
+    public boolean isTokenValid(String token) {
+        try {
+            Claims claims = jwtUtil.extractClaims(token);
+            return !claims.getExpiration().before(new Date());
+        } catch (JwtException e) {
+            return false;
+        }
     }
 
 }
