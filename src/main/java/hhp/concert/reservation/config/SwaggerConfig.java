b7fce329a23f8009b9976f5069745880d17522fc
Linverno <fatal_linverno@naver.com>
2024-10-17 03:02:44 +0900

    SwaggerConfig 추가

---
+++ src/main/java/hhp/concert/reservation/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package hhp.concert.reservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("hhp.concert.reservation")) // 패키지 경로를 API 경로로 설정
+                .build();
+    }
+
+}
