a0fafdd6b86e44a1ed9cdc76efe231de6f0534e0
Linverno <fatal_linverno@naver.com>
2024-10-18 00:01:52 +0900

    최상위로 이동

--- src/main/java/hhp/concert/reservation/HhplusConcertReservationApplication.java
+++ src/main/java/hhp/concert/reservation/HhplusConcertReservationApplication.java
@@ -1,8 +1,7 @@
-package hhp.concert.reservation.hhplusconcertreservation;
+package hhp.concert.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class HhplusConcertReservationApplication {
