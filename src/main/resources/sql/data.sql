4e10f29375d4f2a7d1a319768e4b8104abc8f2b5
Linverno <fatal_linverno@naver.com>
2024-10-18 03:27:00 +0900

    쿼리 데이터 insert

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,61 @@
+INSERT INTO user_info (pay, user_name) VALUES (5000, 'user1');
+INSERT INTO user_info (pay, user_name) VALUES (15000, 'user2');
+
+INSERT INTO concert (concert_name, concert_date) VALUES ('LKR', '2024-10-20');
+INSERT INTO concert (concert_name, concert_date) VALUES ('LEEKR', '2024-10-21');
+
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (1, 1, true);
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (2, 1, true);
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (3, 1, true);
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (4, 1, false);
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (5, 1, false);
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (6, 1, true);
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (7, 1, true);
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (8, 1, true);
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (9, 1, false);
+INSERT INTO seat (seat_Number, reservation_Id, is_Available) VALUES (10, 1, false);
+
+
+/*INSERT INTO RESERVE (IS_TEMPORARY, RESERVATION_DATE, CONCERT_ID, EXPORATION_TIME, RESERVATION_ID, SEAT_ID, USER_ID) VALUES (TRUE, '2024-10-20', 1, '2024-10-20', 1, 1, 1)*/
+
+/*
+INSERT INTO user_info (user_id, password) VALUES ('user1', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user2', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user3', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user4', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user5', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user6', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user7', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user8', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user9', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user10', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user11', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user12', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user13', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user14', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user15', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user16', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user17', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user18', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user19', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user20', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user21', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user22', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user23', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user24', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user25', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user26', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user27', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user28', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user29', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user30', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user31', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user32', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user33', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user34', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user35', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user36', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user37', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user38', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user39', '1234');
+INSERT INTO user_info (user_id, password) VALUES ('user40', '1234');*/
