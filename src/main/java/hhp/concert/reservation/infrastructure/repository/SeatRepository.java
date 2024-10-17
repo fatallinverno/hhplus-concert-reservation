5b8244b6dc342a94b6463d392c86ea15413f9a93
Linverno <fatal_linverno@naver.com>
2024-10-17 15:17:21 +0900

    좌석 선택 후 예약 시, 비관적 락 사용하여 동시성 제어 적용

--- src/main/java/hhp/concert/reservation/infrastructure/repository/SeatRepository.java
+++ src/main/java/hhp/concert/reservation/infrastructure/repository/SeatRepository.java
@@ -1,7 +1,15 @@
 package hhp.concert.reservation.infrastructure.repository;
 
 import hhp.concert.reservation.domain.entity.SeatEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface SeatRepository extends JpaRepository<SeatEntity, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM SeatEntity s WHERE s.seatId = :seatId")
+    Optional<SeatEntity> findByIdForReservation(Long seatId);
 }
